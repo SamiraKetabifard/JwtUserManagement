83161469003838b21a59c9530d49289c8c494de9
SamiraKetabifard <samiraketabi.fard@gmail.com>
2025-04-17 14:32:40 +0330

    del import

--- src/test/java/com/example/jwtusermanagement/controller/UserControllerTest.java
+++ src/test/java/com/example/jwtusermanagement/controller/UserControllerTest.java
@@ -8,12 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
